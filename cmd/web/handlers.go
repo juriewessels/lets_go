b65132de9bd0a0d55f98388502f62d9142fb957c
Jurie Wessels <jurie@juriewessels.com>
2018-10-19 15:37:44 -0700

    2.5. Routing Requests

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,3 +12,11 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Hello from Snippetbox"))
 }
+
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Display a specific snippet..."))
+}
+
+func NewSnippet(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Display the new snippet form...."))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
+  mux.HandleFunc("/snippet", ShowSnippet)
+  mux.HandleFunc("/snippet/new", NewSnippet)
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
